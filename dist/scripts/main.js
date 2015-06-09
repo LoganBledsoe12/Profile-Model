e0528fa7dff10401f7ef18a22cd5d241844c3a8c
Logan Bledsoe <lbledsoe12@hotmail.com>
2015-06-08 20:55:19 -0500

    complete

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,8 +1,11 @@
 var user = new UserModel();
+var spnname = $('#spnname');
+var spnrole = $('#spnrole');
 var App = Backbone.Router.extend({
+
 	routes: {
-		'': 'profile',
-		'edit': 'edit'
+		'': 'home',
+		'edit': 'edit',
 	},
 	profile: function() {
 		$('.page').hide();
@@ -11,8 +14,39 @@ var App = Backbone.Router.extend({
 	edit: function() {
 		$('.page').hide();
 		$('#edit').show();
-	}
+	},
+	home: function(){
+		$('.page').hide();
+		$('#home').show();
+			}
 });
+$('#name').val (user.get('name'));
+$('#inputEmail3').val (user.get('email'));
+$('#role').val (user.get('role'));
+// $('#inputPassword3').val (user.get('name'));
 
 var app = new App();
 Backbone.history.start();
+
+$('#edituser').submit(inputform);
+
+	function inputform(event){
+		console.log (inputform)
+		event.preventDefault();
+		user.set({'name':$('#name').val()})
+		user.set({'email':$('#inputEmail3').val()})
+		user.set({'role':$('#role').val()})
+		// $('#inputbox').val();
+		// console.log ($('#inputbox').val());
+		// var goToPage='search/'+($('#inputbox').val());
+		// console.log (goToPage)
+		// myRouter.navigate(goToPage,{trigger:true})
+
+	}
+user.on('change', function(newModel){
+		console.log(newModel)
+		spnname.text(user.get('name'));
+		spnrole.text(user.get('role'));
+		console.log('change')
+
+	});
